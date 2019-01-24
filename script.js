f4da5bbebfa077da1bae174e79b9bbdd3c722d66
Rayna Carter <RaynaCarter@students.berkeley.net>
2019-01-24 12:34:33 -0800

    //

--- script.js
+++ script.js
@@ -1,25 +1,37 @@
 
 function run(){
 
-    var tigger = new Tiger("Tigger");
+    var tigger = new Tiger("Tigger","meat");
     tigger.eat("meat");
-    tigger.eat("kibble");
+    //tigger.eat("kibble");
 
-    var pooh = new Bear("Pooh");
+    var pooh = new Bear("Pooh","fish");
     pooh.eat("fish");
     pooh.eat("meat");
+
+    var unicorn = new Unicorn("Unicorn","marshmallows");
+    unicorn.eat("fish");
+    unicorn.eat("meat");
+
+    var giraffes = new Giraffes("Giraffes","leaves");
+    giraffes.eat("fish");
+    giraffes.eat("meat");
+
+    var bee = new Bee("Bee","leaves");
+    bee.eat("fish");
+    bee.eat("meat");
 
 
 }
 
-class Tiger {
+class Animal {
 
-    constructor(tName) {
+    constructor(tName, tfavfood) {
         this.name=tName;
-        this.favoriteFood = "meat";
+        this.favoriteFood = tfavfood;
     }
     //method #1
-   sleep(){
+    sleep(){
         console.log(this.name +" sleeps for 8 hours");
     }
 
@@ -28,26 +40,118 @@ class Tiger {
         console.log(this.name+ " eats " +food);
         food!=this.favoriteFood ?this.sleep():   console.log("YUM!!! "+this.name+" wants more "+ food);
     }
-
 }
 
-class Bear{
 
-    constructor(oName) {
-        this.name=oName;
-        this.favoriteFood = "fish";
+
+
+
+class Tiger extends Animal {
+
+    constructor(tName) {
+        super("Tiger");
+        this.name=tName;
+        this.favoriteFood = "meat";
     }
 
 
 
-    //method #1
-    /*sleep(){
+}
+
+
+
+
+
+class Bear extends Animal{
+
+    constructor(tName, tfavfood) {
+        super("Bear");
+        this.name=tName;
+        this.favoriteFood = tfavfood;
+    }
+
+    sleep(){
         console.log(this.name +" hibernates for 4 months");
     }
 
-    //method #2
-   // eat(food){
+    /*eat(food){
         console.log(this.name+ " eats " +food);
         food!=this.favoriteFood ?this.sleep():   console.log("YUM!!! "+this.name+" wants more "+ food);
-   } */
+   }*/
+
 }
+
+
+
+
+
+
+
+
+class Unicorn extends Animal{
+
+    constructor(tName, tfavfood) {
+        super("Unicorn");
+        this.name=tName;
+        this.favoriteFood = tfavfood;
+    }
+
+    sleep(){
+        console.log(this.name +" sleeps in a cloud");
+    }
+
+    /*eat(food){
+        console.log(this.name+ " eats " +food);
+        food!=this.favoriteFood ?this.sleep():   console.log("YUM!!! "+this.name+" wants more "+ food);
+   }*/
+
+}
+
+
+
+
+
+
+class Giraffes extends Animal{
+
+    constructor(tName, tfavfood) {
+        super("Giraffes");
+        this.name=tName;
+        this.favoriteFood = tfavfood;
+    }
+
+    sleep(){
+        console.log(this.name +" sleeps in a cloud");
+    }
+
+    eat(food){
+        console.log(this.name+ " eats " +food);
+        food!=this.favoriteFood ?this.sleep():   console.log("YUM!!! "+this.name+" wants more leaves");
+   }
+
+}
+
+
+
+
+
+
+class Bee extends Animal{
+
+    constructor(tName, tfavfood) {
+        super("Bee");
+        this.name=tName;
+        this.favoriteFood = tfavfood;
+    }
+
+    sleep(){
+        console.log(this.name +" sleeps in a cloud");
+    }
+
+    /*eat(food){
+        console.log(this.name+ " eats " +food);
+        food!=this.favoriteFood ?this.sleep():   console.log("YUM!!! "+this.name+" wants more "+ food);
+   }*/
+
+}
+
